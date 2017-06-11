d29e4cbe3e13f17b16b3ac0da44d80c6b9484784
tedlopez <tedmlopez@gmail.com>
2017-06-11 14:13:01 -0400

    Created function to retrieve user info

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require "httparty"
+require "json"
 
 class Kele
   include HTTParty
@@ -8,6 +9,11 @@ class Kele
     puts response.code
     raise "Invalid email or password" if response.code == 404
     @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get(base_api_endpoint("users/me"), headers: { "authorization" => @auth_token })
+    @user_data = JSON.parse(response.body)
   end
 
 private
